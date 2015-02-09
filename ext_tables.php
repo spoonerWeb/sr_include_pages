96989f85c2b6a27dc00fed13767fe336d407c374
Thomas Löffler <loeffler@spooner-web.de>
2015-02-09 11:02:30 +0100

    [TASK] Makes it compatible to TYPO 6.2
    * Uses TYPO3 CGL

--- ext_tables.php
+++ ext_tables.php
@@ -1,10 +1,11 @@
 <?php
-if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
-$tempColumns = Array (
-	'tx_srincludepages_pages' => Array (
+defined ('TYPO3_MODE') || die ('Access denied.');
+
+$tempColumns = array (
+	'tx_srincludepages_pages' => array (
 		'exclude' => 0,
 		'label' => 'LLL:EXT:sr_include_pages/locallang_db.php:tt_content.tx_srincludepages_pages',
-		'config' => Array (
+		'config' => array (
 			'type' => 'group',
 			'internal_type' => 'db',
 			'allowed' => 'pages',
@@ -14,23 +15,29 @@ $tempColumns = Array (
 			'show_thumbs' => 1,
 		)
 	),
-	'tx_srincludepages_column' => Array (),
+	'tx_srincludepages_column' => array(),
 );
 
 
-t3lib_div::loadTCA('tt_content');
-t3lib_extMgm::addTCAcolumns('tt_content',$tempColumns,1);
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tt_content', $tempColumns, 1);
 $TCA['tt_content']['columns']['tx_srincludepages_column'] = $TCA['tt_content']['columns']['colPos'];
 $TCA['tt_content']['columns']['tx_srincludepages_column']['exclude'] = 0;
-$TCA['tt_content']['types'][$_EXTKEY.'_pi1']['showitem'] = 'CType;;4;button;1-1-1, 
-	header;;3;;2-2-2, 
+$TCA['tt_content']['types'][$_EXTKEY.'_pi1']['showitem'] = 'CType;;4;button;1-1-1,
+	header;;3;;2-2-2,
 	tx_srincludepages_pages;;;;3-3-3,recursive,tx_srincludepages_column';
 
- 
-$TCA['tt_content']['ctrl']['typeicons'][$_EXTKEY.'_pi1'] = t3lib_extMgm::extRelPath($_EXTKEY).'ext_icon.gif';
 
-t3lib_extMgm::addPlugin(Array('LLL:EXT:sr_include_pages/locallang_db.php:tt_content.CType', $_EXTKEY.'_pi1'),'CType');
+$TCA['tt_content']['ctrl']['typeicons'][$_EXTKEY.'_pi1'] = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY) . 'ext_icon.gif';
 
-if (TYPO3_MODE=='BE')	$TBE_MODULES_EXT['xMOD_db_new_content_el']['addElClasses']['tx_srincludepages_pi1_wizicon'] = t3lib_extMgm::extPath($_EXTKEY).'pi1/class.tx_srincludepages_pi1_wizicon.php';
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(
+	array(
+		'LLL:EXT:sr_include_pages/locallang_db.php:tt_content.CType', $_EXTKEY . '_pi1'
+	),
+	'CType'
+);
+
+if (TYPO3_MODE=='BE') {
+	$TBE_MODULES_EXT['xMOD_db_new_content_el']['addElClasses']['tx_srincludepages_pi1_wizicon'] = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'pi1/class.tx_srincludepages_pi1_wizicon.php';
+}
 ?>
 
